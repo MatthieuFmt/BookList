b25b0b9b432b4dffdda0e3cd341887ca69301071
matthieufmt <matt136@hotmail.fr>
2023-05-09 16:44:09 +0200

    [+] homePage

---
+++ client/src/components/NavbarConnection/NavbarConnection.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div>LOGO</div>
+      <div>
+        <div>Connexion</div>
+        <div>Inscription</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className="container home">
+      <h1 className="home__title">Booklist</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos nulla
+        provident aliquid voluptatem ex molestias dicta, aut itaque. Commodi,
+        nihil!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
